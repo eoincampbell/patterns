e7a408fddcaf57d7e0a12d012803c043727fdacd
eoincampbell <eoin.campbell@gmail.com>
2016-10-16 22:29:12 +0100

    Added Singleton Example

---
+++ src/TryCatchMe.Patterns.Design/Creational/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchMe.Patterns.Design.Creational
+{
+    public class Logger
+    {
+        private static Logger _instance;
+
+        private static object _syncLock = new object();
+
+        protected Logger()
+        {
+            
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Logger();
+                        }   
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
